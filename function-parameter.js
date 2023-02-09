59da1303a851724e240b15ece00566681e047c5d
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-09 20:41:02 +0600

    function-parameter added

---
+++ function-parameter.js
@@ -0,0 +1,27 @@
+function beingSingara(money){
+    console.log('mama singara den...');
+    console.log(money)
+}
+beingSingara();
+
+// --------------------------------------
+
+function add(num1, num2, num3, num4){
+    console.log('going to add:-', num1, num2, num3, num4);
+}
+add(1, 3, 5, 3, 4);
+
+//---------------------------------------
+
+function bringSingara(taka){
+    console.log('TAKA-', taka);
+}
+bringSingara(100);
+
+// --------------------------------------
+
+function bringSingara(taka){
+    console.log('TAKA-', taka);
+}
+var taka = 200;
+bringSingara(taka);
