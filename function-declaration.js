a9fe128fedc3276724ee6019ae036241dffb2c2b
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-09 20:39:32 +0600

    function-declaration added

---
+++ function-declaration.js
@@ -0,0 +1,23 @@
+/*-----------------function declaration--------------------
+
+
+function functionName(parameters){
+    function body
+    return
+}
+var returnedValue = functionName(Parameters value);
+console.log(returnedValue);
+
+-------------------------------------------------------------*/
+
+/* ----------function declaration------------- */
+
+function startFan(){
+    console.log('start up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+
+/* -----------call the function---------------- */
+
+startFan();
