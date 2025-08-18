81d45f7f49c3b9a4a1d210741e3753785cf301e6
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-18 21:32:29 +0600

    task-1 added

---
+++ while/task-1.js
@@ -0,0 +1,15 @@
+let count = 1;
+
+while (count <= 60) {
+    console.log(count, "I will invest at least 6 hrs every single day for next 60 days..!");
+    count++;
+}
+
+
+
+// let num = 1;
+
+// while (num <= 10) {
+//     num++;
+//     console.log('Hello...!');
+// }
