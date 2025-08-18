70b4b3da7acef0cca4a534e0659a36bcb55ac64a
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-18 21:52:16 +0600

    task-5 added

---
+++ while/task-5.js
@@ -0,0 +1,16 @@
+// Implement a countdown timer that counts down from 21 to 15.
+
+// let start = 21;
+// let end = 15;
+
+// while (start >= end) {
+//     let currentNumber = start;
+//     console.log(currentNumber);
+//     start--;
+// }
+
+let i = 21;
+while (i >= 15) {
+    console.log(i);
+    i--;
+}
