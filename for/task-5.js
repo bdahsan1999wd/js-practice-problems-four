c7e4b2a88260670c2399c808262a0b375a7c3991
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-18 20:16:27 +0600

    task-5 added

---
+++ for/task-5.js
@@ -0,0 +1,14 @@
+// Implement a countdown timer that counts down from 81 to 65.
+
+// let start = 81;
+// let end = 65;
+
+// for (let i = start; i >= end; i--) {
+//     let currentNumber = i;
+//     console.log(currentNumber);
+// }
+
+
+for (let i = 81; i >= 65; i--) {
+    console.log(i);
+}
