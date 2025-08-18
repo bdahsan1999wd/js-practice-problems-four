1f0c141ac15961418cd5315dae89cc1855270baf
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-18 13:47:50 +0600

    js-array-looping-task added

--- problem-2.js
+++
@@ -1,7 +0,0 @@
-function make_avg(num1, num2, num3){
-    const total = num1 + num2 + num3;
-    const average = total / 3;
-    return average;
-}
-const averageValues = make_avg(1, 2, 3);
-console.log('Average Number:-', averageValues);
