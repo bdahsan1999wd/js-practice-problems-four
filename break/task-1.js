f97f77f307cc1a6bc9ca1466e2a793b9b027a545
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-18 22:22:47 +0600

    task-1 added

---
+++ break/task-1.js
@@ -0,0 +1,40 @@
+// Write (for loop) 1 to 200. Use (break) to exit the loop once you find 100.
+for (let i = 1; i <= 200; i++) {
+    console.log('for loop:', i);
+    if (i === 100) {
+        break;
+    }
+}
+
+// ------------------------------------------------------
+// for (let money = 50; money <= 150; money++) {
+//     console.log(money);
+//     if (money === 100) {
+//         break;
+//     }
+// }
+
+
+console.log('--------------------------------------');
+
+
+// Write (while loop) 1 to 200. Use (break) to exit the loop once you find 100.
+let i = 1;
+
+while (i <= 200) {
+    console.log('while loop:', i);
+    if (i === 100) {
+        break;
+    }
+    i++;
+}
+
+// ------------------------------------------------------
+// let num = 5;
+// while (num <= 15) {
+//     console.log(num);
+//     if (num === 10) {
+//         break;
+//     }
+//     num++;
+// }
