511820356c4e188043dbcc3962c2820d2047c18a
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-19 12:50:39 +0600

    js-string-looping task-1 Added

---
+++ string/task-1.js
@@ -0,0 +1,56 @@
+// --------------------for loop--------------------
+
+const text = "Bangladesh";
+
+// Initialize counter for counting 'a'
+let count = 0;
+
+for (let i = 0; i < text.length; i++) {
+
+    // Get the current character
+    let currentChar = text[i];
+    // console.log(currentChar);
+
+    // Convert the character to lowercase
+    let lowerChar = currentChar.toLowerCase();
+    // console.log(lowerChar);
+
+    // Check if the character is 'a'
+    if (lowerChar === 'a') {
+        // Increase the count by 1
+        count++;
+    }
+}
+console.log(count);
+
+
+console.log('-----------------');
+
+
+// --------------------while loop--------------------
+
+const text2 = "Bandarban";
+
+// Initialize counter for counting 'a' or 'A'
+let count2 = 0;
+
+let i = 0;
+
+while (i < text2.length) {
+
+    // Get the current character
+    let currentChar = text2[i];
+    // console.log(currentChar);
+
+    // Convert the character to lowercase
+    let lowerChar = currentChar.toLowerCase();
+    // console.log(lowerChar);
+
+    // Check if the character is 'a'
+    if (lowerChar === 'a') {
+        // Increase the count by 1
+        count2++;
+    }
+    i++;
+}
+console.log(count2);
